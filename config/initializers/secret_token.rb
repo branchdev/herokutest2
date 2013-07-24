58a7a7256e241c00f89d89d78d657e17640a63c5
Mike Branchini <mikebranchini@gmail.com>
2013-07-23 21:38:30 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuTEST2::Application.config.secret_key_base = '76bc8585b0b6b02296beb4b18d1e18df4b43bb777540751fc66a892b109f89633f474a46020b662ddf6d48b724a19b85750b02d99eff36f78782b704f521df8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HerokuTEST2::Application.config.session_store :cookie_store, key: '_herokuTEST2_session'
